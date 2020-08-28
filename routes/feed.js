9d32c6a67dbce8b8a116de0380a7cefdd4d5d9d9
MS <hercules11031998@gmail.com>
2020-08-28 12:47:44 +0300

    starter setup

---
+++ routes/feed.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+const FeedController = require('../controllers/feed');
+
+router.get('/posts',  FeedController.getPosts);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express();
 
+const FEED_Route = require('./routes/feed');
+
+app.use(FEED_Route);
 
 app.listen(8080);
